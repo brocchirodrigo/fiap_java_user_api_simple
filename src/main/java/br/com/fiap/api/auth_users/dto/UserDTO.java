e39aca81174427d51a24097e3da861deefaea24e
brocchirodrigo <rodrigo.brocchi@gmail.com>
2024-05-22 23:18:43 -0300

    feat: Bean Validation CPF and birthDate

--- src/main/java/br/com/fiap/api/auth_users/dto/UserDTO.java
+++ src/main/java/br/com/fiap/api/auth_users/dto/UserDTO.java
@@ -2,6 +2,8 @@ package br.com.fiap.api.auth_users.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.PastOrPresent;
+import org.hibernate.validator.constraints.br.CPF;
 
 import java.time.LocalDate;
 
@@ -14,8 +16,10 @@ public record UserDTO(
     @Email(message = "Invalid e-mail.")
     String email,
 
+    @CPF(message = "Brazilian CPF not valid.")
     String cpf,
 
+    @PastOrPresent(message = "Invalid date.")
     LocalDate birthDate
 ) {
 }
